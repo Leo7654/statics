d8d3372b76e9a234bdb7299ed2c41e6b4c397342
Leo <leo.lee7654@gmail.com>
2017-01-19 01:38:03 +0900

    Update index.js

--- curtain/index.js
+++ curtain/index.js
@@ -1,1 +1,25 @@
 console.log("hello");
+
+let back = $('<button>');
+back.text("back")
+back.on('click', () => {
+  console.log("back");
+  $.get('/b');
+})
+$(document.body).append(back)
+
+let stop = $('<button>');
+stop.text("back")
+stop.on('click', () => {
+  console.log("stop");
+  $.get('/s');
+})
+$(document.body).append(stop)
+
+let forward = $('<button>');
+forward.text("back")
+forward.on('click', () => {
+  console.log("forward");
+  $.get('/f');
+})
+$(document.body).append(forward)
